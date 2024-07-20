68d51020b820609e8e44d48e416424780bc8e5f4
De70 <devanshbhadauria4070@gmail.com>
2024-07-20 17:27:37 +0530

    database

--- Configuration/Database.js
+++ Configuration/Database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/moneyLendingDB', {
+        await mongoose.connect('mongodb://localhost:27017/moneyLending', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
